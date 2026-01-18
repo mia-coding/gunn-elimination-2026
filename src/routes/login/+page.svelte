02e2332398128f91bb815553469d0ff3b977f5b9
mia <138834977+mia-coding@users.noreply.github.com>
2026-01-18 01:51:39 -0800

    Update +page.svelte
    revert changes

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -23,7 +23,7 @@
 		const { data, error } = await supabase.auth.signInWithOAuth({
 			provider: 'google',
 			options: {
-				redirectTo: `${window.location.origin}/api/auth/callback`,
+				redirectTo: `${url}/api/auth/callback`,
 				scopes: 'openid profile email',
 			}
 		});
