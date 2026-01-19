f68e13e69e9f19ea4674755dd921c1f8546cbb4d
mia <138834977+mia-coding@users.noreply.github.com>
2026-01-18 19:44:15 -0800

    Update +page.svelte
    Updated the rules/calendar to be the 2026 ones, instead of 2025

--- src/routes/app/rules/+page.svelte
+++ src/routes/app/rules/+page.svelte
@@ -2,14 +2,14 @@
 	import Container from '$lib/components/Container.svelte';
 	import YoutubeIcon from '$lib/icons/YoutubeIcon.svelte';
 
-	import calendarImage from '$lib/assets/images/calendar-2025.png';
+	import calendarImage from '$lib/assets/images/elimination-calendar.png';
 </script>
 
 <Container class="">
 	<h1 class="mb-8 text-3xl">Rules</h1>
 	<div class="flex flex-col items-start justify-center space-y-4">
 		<a
-			href="https://docs.google.com/document/d/1uJEGcxu-WBbnwJSGYhfKRzyuKRkAJNGht6FCeofEMvA/edit?usp=sharing"
+			href="https://docs.google.com/document/d/1UXb95TSkPEwlsb6jPRCTJyQ57xhrqAm3R6lBBDKE7jg/edit?pli=1&tab=t.0"
 			target="_blank"
 			class="my-2 rounded-lg underline hover:text-neutral-400">All Rules and Game Calendar</a
 		>
