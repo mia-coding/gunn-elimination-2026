87e1cb32b2b84b2b03c7937744d54707465d2546
mia <138834977+mia-coding@users.noreply.github.com>
2026-01-18 19:58:09 -0800

    Fixed rules document link

--- src/routes/app/rules/+page.svelte
+++ src/routes/app/rules/+page.svelte
@@ -9,7 +9,7 @@
 	<h1 class="mb-8 text-3xl">Rules</h1>
 	<div class="flex flex-col items-start justify-center space-y-4">
 		<a
-			href="https://docs.google.com/document/d/1UXb95TSkPEwlsb6jPRCTJyQ57xhrqAm3R6lBBDKE7jg/edit?pli=1&tab=t.0"
+			href="https://docs.google.com/document/d/1uJEGcxu-WBbnwJSGYhfKRzyuKRkAJNGht6FCeofEMvA/edit?tab=t.0"
 			target="_blank"
 			class="my-2 rounded-lg underline hover:text-neutral-400">All Rules and Game Calendar</a
 		>
