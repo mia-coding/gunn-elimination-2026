9fe3122628afa951d30bb04066e600f0f8e8a790
mia <138834977+mia-coding@users.noreply.github.com>
2026-01-29 20:55:33 -0800

    Update +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -9,16 +9,13 @@
 	</h1>
 	<div class="mt-4 flex flex-col space-y-2 rounded-lg p-4 text-center text-neutral-300">
 		<div class="text-xl">
-			For reporting, <b>please CC the other person</b> in your email. Remember to read all rules before
-			sending us anything.
+			For reporting, questions, or concerns, please fill out this form, and remember to read all rules before sending us anything.
 		</div>
 		<div class="rounded-lg bg-neutral-900 px-4 py-2"><a
 			href="https://forms.gle/7CHxeWRAjVL2MrRF6"
 			target="_blank"
-			class="rounded-lg px-4 py-2 underline hover:text-neutral-400">Fill out this form for questions or concerns: https://forms.gle/7CHxeWRAjVL2MrRF6</a
+			class="rounded-lg px-4 py-2 underline hover:text-neutral-400">https://forms.gle/7CHxeWRAjVL2MrRF6</a
 		></div>
-		<div class="rounded-lg bg-neutral-900 px-4 py-2">Mia Yair: my40666@pausd.us</div>
-		<div class="rounded-lg bg-neutral-900 px-4 py-2">Natalie Chan: nc34609@pausd.us</div>
 	</div>
 </Container>
 <Footer />
